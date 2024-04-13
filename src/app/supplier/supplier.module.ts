629aa0da780930549d3c823dfc5fbb43f02583d0
Anubhav <proanubhav@gmail.com>
2024-04-13 19:28:54 +0530

    add module

---
+++ src/app/supplier/supplier.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddSupplierComponent } from './add-supplier/add-supplier.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddSupplierComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SupplierModule { }
